c696850bc3560ef7ed0ffe57949f29c9e8751004
LameesMomen <lameeszakialabd@gmail.com>
2023-04-20 18:00:35 +0200

    psuh

--- forms/contact.php
+++ forms/contact.php
@@ -7,27 +7,35 @@
     die( 'Unable to load the "PHP Email Form" Library!');
   }
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
+  $to = "lameeszakialabd@gmail.com";
+  $subject = $_POST['subject'];
+  $txt = $_POST['message'];
+  $email = $_POST['email'];
+  $headers = "From:" . $email . "\r\n";
+
+  mail($to,$subject,$txt,$headers);
+
+  // $contact = new PHP_Email_Form;
+  // $contact->ajax = true;
   
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+  // $contact->to = $receiving_email_address;
+  // $contact->from_name = $_POST['name'];
+  // $contact->from_email = $_POST['email'];
+  // $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   
-  $contact->smtp = array(
-    'host' => 'https://bram-hammam.vercel.app/',
-    'username' => 'LameesZaki',
-    'password' => 'LameesZaki@10',
-    'port' => '587'
-  );
+  // $contact->smtp = array(
+  //   'host' => 'https://bram-hammam.vercel.app/',
+  //   'username' => 'LameesZaki',
+  //   'password' => 'LameesZaki@10',
+  //   'port' => '587'
+  // );
 
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+  // $contact->add_message( $_POST['name'], 'From');
+  // $contact->add_message( $_POST['email'], 'Email');
+  // $contact->add_message( $_POST['message'], 'Message', 10);
 
-  echo $contact->send();
+  // echo $contact->send();
 ?>
